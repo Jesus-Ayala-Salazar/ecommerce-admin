450564d0beccc241adf2db15dc245dd9f219be82
jameshsiao2025 <42394457+jameshsiao2025@users.noreply.github.com>
2021-06-04 11:02:22 -0700

    Add files via upload

--- app/models/product.model.js
+++ app/models/product.model.js
@@ -1,9 +1,5 @@
 module.exports = (sequelize, Sequelize) => {
     const Product = sequelize.define("product", {
-      productId: {
-        type: Sequelize.INTEGER,
-        primaryKey: true
-      },
       description: {
         type: Sequelize.STRING
       },
