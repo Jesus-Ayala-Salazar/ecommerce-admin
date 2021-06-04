21885358e1e314684d1a82e3cf25bdfc3837ac1a
Mia Vu <melk1359@yahoo.com>
2021-06-04 06:09:37 -0700

    created product specific components for client side

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: "localhost",
   USER: "root",
-  PASSWORD: "password",
+  PASSWORD: "mv@72137628",
   DB: "testdb",
   dialect: "mysql",
   pool: {
---
+++ react-crud-web-api-master/src/components/add-product.component.js
@@ -0,0 +1,135 @@
+import React, { Component } from "react";
+import ProductDataService from "../services/product.service";
+
+export default class AddProduct extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeName = this.onChangeName.bind(this);
+    this.onChangePrice = this.onChangePrice.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.saveProduct = this.saveProduct.bind(this);
+    this.newProduct = this.newProduct.bind(this);
+
+    this.state = {
+      productId: null,
+      name: "",
+      description: "", 
+      inStock: false,
+
+      price: 0.00
+    };
+  }
+
+  onChangeName(e) {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  onChangePrice(e) {
+    this.setState({
+      price: e.target.value
+    });
+  }
+
+  saveProduct() {
+    var data = {
+      name: this.state.name,
+      description: this.state.description
+    };
+
+    ProductDataService.create(data)
+      .then(response => {
+        this.setState({
+          productId: response.data.productId,
+          name: response.data.name,
+          description: response.data.description,
+          inStock: true,
+
+          price: response.data.price
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  newProduct() {
+    this.setState({
+      id: null,
+      name: "",
+      description: "",
+      inStock: false,
+
+      price: 0.00
+    });
+  }
+
+  render() {
+    return (
+      <div className="submit-form">
+        {this.state.submitted ? (
+          <div>
+            <h4>You submitted successfully!</h4>
+            <button className="btn btn-success" onClick={this.newProduct}>
+              Add
+            </button>
+          </div>
+        ) : (
+          
+          <div>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="name"
+                required
+                value={this.state.name}
+                onChange={this.onChangeName}
+                name="name"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="description">Description</label>
+              <input
+                type="text"
+                className="form-control"
+                id="description"
+                required
+                value={this.state.description}
+                onChange={this.onChangeDescription}
+                name="description"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="price">Price</label>
+              <input
+                type="number"
+                className="form-control"
+                id="price"
+                required
+                value={this.state.price}
+                onChange={this.onChangePrice}
+                name="price"
+              />
+            </div>
+
+            <button onClick={this.saveProduct} className="btn btn-success">
+              Submit
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
